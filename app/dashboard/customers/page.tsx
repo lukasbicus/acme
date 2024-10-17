5c3727f6b830b9c714cb6f1d61164b063b085b64
lukasbicus <ing.lukas.bicus@gmail.com>
2024-10-17 10:53:45 +0200

    Add customers.

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,33 @@
+import {fetchFilteredCustomers} from "@/app/lib/data";
+import CustomersTable from "@/app/ui/customers/table";
+import {lusitana} from "@/app/ui/fonts";
+import Search from "@/app/ui/search";
+import {InvoicesTableSkeleton} from "@/app/ui/skeletons";
 import {Metadata} from "next";
+import {Suspense} from "react";
 
 export const metadata: Metadata = {
   title: 'Customers',
 };
 
-export default function Page() {
-  return <p>Customers Page</p>;
+export default async function Page({searchParams}: {
+  searchParams: {
+    query?: string
+  }
+}) {
+  const query = searchParams?.query || '';
+  const customers = await fetchFilteredCustomers(query)
+  return (
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className={`${lusitana.className} text-2xl`}>Customers</h1>
+      </div>
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        <Search placeholder="Search invoices..."/>
+      </div>
+      <Suspense key={query} fallback={<InvoicesTableSkeleton/>}>
+        <CustomersTable customers={customers}/>
+      </Suspense>
+    </div>
+  )
 }
