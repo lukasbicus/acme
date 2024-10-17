62eaed241d41867a558836d50a1734ba7872a2ae
lukasbicus <ing.lukas.bicus@gmail.com>
2024-10-17 10:28:27 +0200

    Add metadataBase

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ export const metadata: Metadata = {
     default: 'Acme Dashboard',
   },
   description: 'The official Next.js Course Dashboard, built with App Router.',
-  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+  metadataBase: new URL('https://acme-peach-gamma.vercel.app/'),
 };
 
 export default function RootLayout({
